14d0ae45cc9f16307a426e7325796fbc2ac1e812
Kalyan <kalyanrko14@gmail.com>
2022-05-06 23:54:58 +0530

    SignupPage

--- loanapp/src/SignUpScreen.js
+++ loanapp/src/SignUpScreen.js
@@ -1,18 +1,33 @@
 import React from "react";
 import { useState } from "react";
+import validator from "validator";
 import classes from "./SignUpScreen.module.css";
 
 function SignUpScreen() {
   const [Email, setEmail] = useState("");
   const [password, setpassword] = useState("");
+  const [messageEmail, setmessageEmail] = useState("");
+  const [messagePassword, setmessagePassword] = useState("");
+
   const handleEmail = (e) => {
     setEmail(e.target.value);
+    if (validator.isEmail(Email)) {
+      setmessageEmail("Valid");
+    } else {
+      setmessageEmail("NOT A VALID EMAIL");
+    }
   };
   const handlePassword = (e) => {
     setpassword(e.target.value);
+    if (password.length < 6) {
+      setmessagePassword("password must be at least 6 characters");
+    } else {
+      setmessagePassword("valid");
+    }
   };
   const handleSubmit = (e) => {
     console.log("hi");
+    e.preventDefault();
   };
   return (
     <div className={classes.Main}>
@@ -28,6 +43,7 @@ function SignUpScreen() {
             placeholder="username"
             onChange={handleEmail}
           />
+          {messageEmail}
           <br></br>
           <label>password</label>
           <input
@@ -36,6 +52,7 @@ function SignUpScreen() {
             placeholder="username"
             onChange={handlePassword}
           />
+          {messagePassword}
           <br></br>
           <button onClick={handleSubmit}>SignUp</button>
         </form>
