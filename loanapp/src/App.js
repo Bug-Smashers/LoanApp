ee931d66e156d357aaa7a669768e60aaae047e63
SIVAGANESH08 <60740604+SIVAGANESH08@users.noreply.github.com>
2022-05-08 00:16:04 +0530

    userin added

--- loanapp/src/App.js
+++ loanapp/src/App.js
@@ -3,13 +3,14 @@ import "./App.css";
 import LoginScreen from "./LoginScreen";
 import SignUpScreen from "./SignUpScreen";
 import Userdata from "./Userdata";
-
+import Document from "./Document";
+import UserIn from "./UserIn";
 function App() {
   return (
     <>
       {" "}
       <div>
-        <Userdata />
+        <UserIn />
       </div>
     </>
   );
---
+++ loanapp/src/UserIn.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Document from './Document';
+
+function UserIn() {
+    return (
+        <>
+            <Document />
+            <Document/>
+        </>
+    );
+};
+
+export default UserIn;
