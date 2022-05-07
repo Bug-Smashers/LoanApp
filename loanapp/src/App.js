d46c4c7e53ef00356f19ce62b89fbb3733c19aac
Kalyan <kalyanrko14@gmail.com>
2022-05-07 18:00:30 +0530

    UserDataInput

--- loanapp/src/App.js
+++ loanapp/src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css";
 import LoginScreen from "./LoginScreen";
 import SignUpScreen from "./SignUpScreen";
+import Userdata from "./userdatainput";
 
 function App() {
   return (
     <>
       {" "}
       <div>
-        <SignUpScreen />
+        <Userdata />
       </div>
     </>
   );
