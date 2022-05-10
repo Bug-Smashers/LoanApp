fc21f49e9be60339fd6329670987e74a2001a27d
Kalyan <kalyanrko14@gmail.com>
2022-05-10 16:47:04 +0530

    .

--- laonAppApi/index.js
+++ laonAppApi/index.js
@@ -1,110 +1,106 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const uploade = require("express-fileupload");
 const mongoose = require("mongoose");
 const User = require("./models/user");
-const user1=User.schemadetail;
-const userdetail=require('./userdetail');
-const userPersonaldetails=require("./models/userPersonaldetails")
-const firstdetail=userPersonaldetails.firstdetail;
+const user1 = User.schemadetail;
+const userdetail = require("./userdetail");
+const userPersonaldetails = require("./models/userPersonaldetails");
+const firstdetail = userPersonaldetails.firstdetail;
 
-const aadhardetail=userPersonaldetails.seconddetail;
-const pandetail=userPersonaldetails.thirddetail;
-const thirddetail=userPersonaldetails.forthdetail;
-const upload =require("./middleware/upload");
-
+const aadhardetail = userPersonaldetails.seconddetail;
+const pandetail = userPersonaldetails.thirddetail;
+const thirddetail = userPersonaldetails.forthdetail;
+const upload = require("./middleware/upload");
 
 app.use(cors());
+app.use(uploade());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://loanapi:loanapi@cluster0.sdy8u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority").then(()=>{
-  console.log("db is connected")
-})
+mongoose
+  .connect(
+    "mongodb+srv://loanapi:loanapi@cluster0.sdy8u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("db is connected");
+  });
 
-app.get("/getusers",userdetail.fetchData)
+app.get("/getusers", userdetail.fetchData);
 
 app.post("/register", async (req, res, next) => {
-  console.log(user1)
-  const {username}=req.body;
-  const {password}=req.body;
+  console.log(user1);
+  const { username } = req.body;
+  const { password } = req.body;
   // username: req.body.Email,
   // console.log(username)
   //     password: req.body.password,
 
- 
   try {
-
-    const user =new  user1({username,password});
+    const user = new user1({ username, password });
     await user.save();
-   return  res.json(await user1.find());
+    return res.json(await user1.find());
   } catch (err) {
     res.json({ status: "error" });
   }
 });
 
-
-app.post('/personal', async(req,res)=>{
-  const {name}=req.body;
-  const {email}=req.body;
-  const {aadharnumber}=req.body;
-  const {pannumber}=req.body;
+app.post("/personal", async (req, res) => {
+  const { name } = req.body;
+  const { email } = req.body;
+  const { aadharnumber } = req.body;
+  const { pannumber } = req.body;
 
   try {
-
-    const user =new  firstdetail({name,email,aadharnumber,pannumber});
+    const user = new firstdetail({ name, email, aadharnumber, pannumber });
     await user.save();
-   return  res.json(await user1.find());
+    return res.json(await user1.find());
   } catch (err) {
     res.json({ status: "error" });
   }
-  
-  
-})
+});
 
 //for aadhar
-app.post('/aadhar',upload.single("documentaadhar"),(req,res)=>{
+app.post("/aadhar", upload.single("documentaadhar"), (req, res) => {
+  let aadhar = new aadhardetail();
 
-  let aadhar=new aadhardetail();
-
-  if(req.file){
-    aadhar.documentaadhar =req.file.path;
+  if (req.file) {
+    aadhar.documentaadhar = req.file.path;
   }
-  aadhar.save()
-  .then(response=>{
+  aadhar
+    .save()
+    .then((response) => {
       res.json({
-          message:"file is uploaded successfully"
-      })
-  }).catch(error=>{
+        message: "file is uploaded successfully",
+      });
+    })
+    .catch((error) => {
       res.json({
-          message:"an errror occured"
-      })
-  })
-  
-  
-})
+        message: "an errror occured",
+      });
+    });
+});
 
 //for pan
-app.post('/pan',upload.single("documentpan"),(req,res)=>{
-  let pan=new pandetail();
+app.post("/pan", upload.single("documentpan"), (req, res) => {
+  let pan = new pandetail();
 
-  if(req.file){
-      pan.documentpan =req.file.path;
+  if (req.file) {
+    pan.documentpan = req.file.path;
   }
-  pan.save()
-  .then(response=>{
+  pan
+    .save()
+    .then((response) => {
       res.json({
-          message:"file is uploaded successfully"
-      })
-  }).catch(error=>{
+        message: "file is uploaded successfully",
+      });
+    })
+    .catch((error) => {
       res.json({
-          message:"an errror occured"
-      })
-  })
-  
-  
-})
-
-
+        message: "an errror occured",
+      });
+    });
+});
 
 app.listen(3001, () => {
   console.log("at port 3001");
